d9285b7f98a257106c63040d59e157e95f015f23
kreps <kkangro78@gmail.com>
2016-09-30 13:36:55 +0300

    Task mvc controller with thymeleaf templates

---
+++ src/main/java/kreps/controller/TaskController.java
@@ -0,0 +1,14 @@
+package kreps.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TaskController {
+
+
+    @RequestMapping("/task")
+    public String test(){
+        return "tasklist";
+    }
+}
